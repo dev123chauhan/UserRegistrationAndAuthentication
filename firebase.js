988cd5faf2a71e88398b7a62ef0ec0b5200baef2
dev123chauhan <76210158+dev123chauhan@users.noreply.github.com>
2023-09-15 19:28:04 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFqFDGxRGwbBdrR6Gjpp_nY5wOXnPgniQ",
+    authDomain: "user-registration-41a8e.firebaseapp.com",
+    projectId: "user-registration-41a8e",
+    storageBucket: "user-registration-41a8e.appspot.com",
+    messagingSenderId: "389556728942",
+    appId: "1:389556728942:web:aff6a5420f51da9729e27f"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
